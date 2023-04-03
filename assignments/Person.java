d483cdb42e45f4f4e5cad165abc0836e392ad91e
amogh-dongre <amoghdongre16@gmail.com>
2023-04-03 22:28:19 +0530

    changes to person again

--- assignments/Person.java
+++ assignments/Person.java
@@ -1,12 +1,15 @@
 import java.util.* ;
+
+import Person.Gender;
 class Person{
     Gender g1;
     Scanner sc;
     String name;
     int age;
-    Person(String name,int age){
+    Person(String name,int age,Gender g1){
         this.age=age;
         this.name=name;
+        this.g1=g1;
     }
     String getName(){
         return name;
@@ -26,15 +29,22 @@ class Person{
     void setGender(Gender g1){
         this.g1=g1;
     }
+    boolean reducedFare(){
+    if(age < 12 || age > 64){
+     return true;
+    }
+    else{
+     return false;
+    }
 
 }
 class Gender extends Person{
     String gender;
-    Gender(String name,int age){
-        super(name, age);
+    Gender(String name,int age,Gender g1){
+        super(name, age,g1);
     }
-    Gender(String name,int age,String gender){
-        this(name, age);
+    Gender(String name,int age,Gender g1,String gender){
+        this(name, age,g1);
         this.gender=gender;
     }
 }
@@ -42,16 +52,16 @@ class Student extends Person{
     String college;
     double gpa;
     YearInSchool y1;
-    Student(String name,int age){
-        super(name, age);
+    Student(String name,int age,Gender g1){
+        super(name, age,g1);
     }
-    Student(String name,int age,String college,double gpa){
-        this(name, age);
+    Student(String name,int age,Gender g1,String college,double gpa){
+        this(name, age,g1);
         this.college=college;
         this.gpa=gpa;
     }
     String getCollege(){
-        retrun college;
+        return college;
     }
     void setCollege(String college){
         this.college=college;
@@ -71,11 +81,11 @@ class Student extends Person{
 }
 class YearInSchool extends Student{
     int year;
-    YearInSchool(String name,int age,String college,double gpa){
-        super(name, age, college, gpa);
+    YearInSchool(String name,int age,Gender g1,String college,double gpa){
+        super(name, age,g1, college, gpa);
     }
-    YearInSchool(String name,int age,String college,double gpa,int year){
-        this(name, age, college, gpa);
+    YearInSchool(String name,int age,Gender g1,String college,double gpa,int year){
+        this(name, age,g1, college, gpa);
         this.year=year;
     }
 }
@@ -83,11 +93,11 @@ class faculty extends Person{
     String rank;
     String dept;
     double salary;
-    faculty(String name,int age){
-        super(name, age);
+    faculty(String name,int age,Gender g1){
+        super(name, age,g1);
     }
-    faculty(String name,int age,String rank,String dept,double salary){
-     this(name, age);
+    faculty(String name,int age,Gender g1,String rank,String dept,double salary){
+     this(name, age,g1);
      this.rank=rank;
      this.dept=dept;
      this.salary=salary;
@@ -114,11 +124,11 @@ class faculty extends Person{
 class Staff extends Person{
     double salary;
     int vacay;
-    Staff(String name,int age){
-        super(name, age);
+    Staff(String name,int age,Gender g1){
+        super(name, age,g1);
     }
-    Staff(String name,int age,double salary,int vacay){
-        this(name, age);
+    Staff(String name,int age,Gender g1,double salary,int vacay){
+        this(name, age,g1);
         this.salary=salary;
         this.vacay=vacay;
 
@@ -138,16 +148,24 @@ class Staff extends Person{
 }
 class GradStudent extends Student{
     String dept,thesis_topic;
-    GradStudent(String name,int age,String college,double gpa){
-        super(name, age, college, gpa);
+    GradStudent(String name,int age,Gender g1,String college,double gpa){
+        super(name, age,g1, college, gpa);
     }
-    GradStudent(String name,int age,String college,double gpa,aString dept,String thesis_topic){
-        this(name, age, college, gpa);
+    GradStudent(String name,int age,String college,Gender g1,double gpa,String dept,String thesis_topic){
+        this(name, age,g1, college, gpa);
         this.thesis_topic=thesis_topic;
         this.dept=dept;
     }
     public String toString() {
     return "Student department: "+dept+" "+"Student thesis topic "+thesis_topic;
 
+    }
+}
+}
+class driver{
+    public static void main(String[] args) {
+        Gender g2 = new Gender()
+        Person p1 = new Person("Karthik", 20,"male");
+        System.out.println(p1.reducedFare());
     }
 }
